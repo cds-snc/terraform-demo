a689df15298c5d0d00bbb49db7fe021cbd654159
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-07-20 16:56:28 -0700

    Changing terraform module links

--- terraform-remote-state/s3.tf
+++ terraform-remote-state/s3.tf
@@ -1,5 +1,5 @@
 module "log_bucket" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.43//S3"
+  source            = "github.com/cds-snc/terraform-modules//S3?ref=v7.0.0"
   bucket_name       = "unique-bucket-name-for-a-remote-state-demo"
   billing_tag_value = "cds-terraform-demo"
 }
--- terragrunt/aws/bucket/s3.tf
+++ terragrunt/aws/bucket/s3.tf
@@ -1,6 +1,6 @@
 # Create an S3 bucket using the CDS Terraform module
 module "log_bucket" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.43//S3"
+  source            = "github.com/cds-snc/terraform-modules//S3?ref=v7.0.0"
   bucket_name       = "${var.bucket_name}-${var.env}"
   billing_tag_value = var.billing_code
 }
