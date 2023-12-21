5db2e1e7023b47bbbd4897b64d583199e9b26300
Wissam Moussa <wissam.moussa@cds-snc.ca>
2023-12-20 21:52:59 -0500

    renaming files

--- sandbox-website/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "cognito_outside_canada_metric_name" {
-  description = "Forms Builder Login: A metric to track request made outside of Canada."
-  value       = one([for x in aws_wafv2_web_acl.forms_acl.rule : one(x.visibility_config.*.metric_name) if x.name == "AWSCognitoLoginOutsideCanada"])
-}
